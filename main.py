c764857827ec272a4d70bdedf2182b0115834bdc
laztaxon <cello@cello.design>
2024-09-15 03:50:31 -0400

    added asteroids 2024-09-15 3:50 AM

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+from asteroidfield import *
+from asteroid import *
 from constants import *
 from player import *
 
@@ -12,17 +14,32 @@ def main():
 
     # FPS Assignment
     clock = pygame.time.Clock()
+
+    # Groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+    player = Player((SCREEN_WIDTH)/2, (SCREEN_HEIGHT)/2)
+
     dt = 0
 
-    player = Player((SCREEN_WIDTH)/2, (SCREEN_HEIGHT)/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        for obj in updatable:
+            obj.update(dt)
         screen.fill("black")
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60) / 100
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
--- player.py
+++ player.py
@@ -1,7 +1,7 @@
 import pygame
 
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS
+from constants import *
 
 
 class Player(CircleShape):
@@ -20,3 +20,26 @@ class Player(CircleShape):
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        # move up
+        if keys[pygame.K_w]:
+            self.move(dt)
+        # down
+        if keys[pygame.K_s]:
+            self.move(-dt)
+        # left
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        # right
+        if keys[pygame.K_d]:
+            self.rotate(dt)
